6fd5f476c8bd83d83b08f75d2fc28d48dcb506d3
sparssss <windprozx@gmail.com>
2025-10-21 13:49:37 +0800

    Update plogin.php

--- student/plogin.php
+++ student/plogin.php
@@ -54,7 +54,7 @@ $row = mysqli_num_rows($qry);
 			echo
 			"<script language='javascript'>
 				alert('Student does not exist.');
-				window.location='index.php';
+				window.location='login.html';
 			</script>";
 		}
 
