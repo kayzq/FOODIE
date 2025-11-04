4b7acd68081664751c5b829bda5ec986bf60e79b
TheDans <danielharrith15@gmail.com>
2025-11-04 14:13:58 +0800

    UPDATE BLOGOUT PATH

--- staff/logout.php
+++ staff/logout.php
@@ -1,18 +1,6 @@
 <?php
-
-session_start();
-include("studconnection.php");
-
-if(!isset($_SESSION['logoutPermission']) || $_SESSION['logoutPermission'] == 0) //No
-{
-	echo"<script language='javascript'>
-	alert('You must cancel or checkout your order before log out!');window.location='history.back()';</script>";
-}
-else
-{
 	session_start();
 	session_unset();
 	session_destroy();
 	header("Location: /FOODIE/landing-page/index.html");
-}
 ?>
